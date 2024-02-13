78ad6010a03ba695ed520a8766ba50b726ea084e
Daniele Carnauba <dannygoncalves41@gmail.com>
2024-02-13 18:38:28 -0300

    docs: add logs com o resultado dos testes

--- starbugs-cumcuber/features/support/hooks.rb
+++ starbugs-cumcuber/features/support/hooks.rb
@@ -10,3 +10,10 @@ Before do
     @order = OrderPage.new
     @popup = Popup.new  
 end
+
+After do |scenario|
+    if (scenario.failed?)
+        screenshot = page.save_screenshot("logs/screenshots/#{scenario.__id__}.png")
+        attach(screenshot, "image/png", "Screenshot")
+    end
+end
